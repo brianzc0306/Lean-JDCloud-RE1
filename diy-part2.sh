8c39e4e44c175d468903ed2f2a798622619fcf36
brianzc0306 <48046657+brianzc0306@users.noreply.github.com>
2025-04-09 20:57:55 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -21,4 +21,4 @@ sed -i 's/192.168.1.1/192.168.0.1/g' package/base-files/files/bin/config_generat
 #git clone https://github.com/ophub/luci-app-amlogic.git package/lean/luci-app-amlogic
 #git clone https://github.com/xiaorouji/openwrt-passwall-packages.git package/lean/passwall-packages
 #git clone https://github.com/xiaorouji/openwrt-passwall.git package/lean/luci-app-passwall
-#git clone  https://github.com/gdy666/luci-app-lucky.git package/lucky
+#git clone https://github.com/gdy666/luci-app-lucky.git package/lucky
